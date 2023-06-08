9cdd70dc4be2f3b5262cd18b6f0d5d7e51cc9093
sadaqat <msadaqatdev@gmail.com>
2023-06-08 22:31:28 +0500

    voice recoder in react native

---
+++ App.js
@@ -0,0 +1,7 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+import RecorderScreen from './res/assets/Recoder';
+export default function App() {
+  return <View>{<RecorderScreen />}</View>;
+}
